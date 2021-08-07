2adacc921519caba1f2061f51acca9f10933f2e3
Doug Owings <doug@dougowings.net>
2021-08-06 22:56:18 -0700

    add template test files

---
+++ test/spec/types.test.js
@@ -0,0 +1,153 @@
+/**
+ * node-utils-h - types tests
+ *
+ * Copyright (C) 2021 Doug Owings
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+const {expect} = require('chai')
+const {ger} = require('../helpers/index.js')
+
+const {types} = require('../../index.js')
+
+describe('types', () => {
+
+    beforeEach(function () {
+
+    })
+
+    describe('#typeOf', () => {
+
+        it('should ...')
+        it('todo', function () {
+            
+        })
+    })
+
+    describe('Cast', () => {
+
+        describe('#toArray', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+    })
+
+    describe('Is', () => {
+
+        describe('#Array', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#Error', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#Buffer', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#Object', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#Stream', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#String', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#Function', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#Iterable', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#PlainObject', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#ReadableStream', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#WritableStream', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+
+        describe('#WriteableStream', () => {
+
+            it('should ...')
+            it('todo', function () {
+                
+            })
+        })
+    })
+})
