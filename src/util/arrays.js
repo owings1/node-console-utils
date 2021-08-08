338e59ff05fa84c8c73adc05983fc49a4c12c0ff
Doug Owings <doug@dougowings.net>
2021-08-07 17:11:08 -0700

    update array utils

--- src/util/arrays.js
+++ src/util/arrays.js
@@ -1,8 +1,10 @@
 const arrays = {
+
     /**
      * Append all values to an array.
      *
      * @throws {TypeError}
+     *
      * @param {array} The array to push to
      * @param {array} The values to push
      * @return {array} The input array
@@ -12,27 +14,21 @@ const arrays = {
         return arr
     },
 
-   /**
-    * Sum all numbers in the array.
-    *
-    * @throws {TypeError}
-    *
-    * @param {array} The input array
-    * @return {integer} The result sum
-    */
-   sum: function arraySum(arr) {
-       return arr.reduce((acc, cur) => acc + cur, 0)
-   },
+    last: function arrayLast(arr) {
+        return arr[arr.length - 1]
+    },
 
-   unique: function arrayUnique(...arrs) {
-       return Object.keys(
-           Object.fromEntries(
-               args.map(Object.values).flat().map(value =>
-                   [value, true]
-               )
-           )
-       )
-   },
+    /**
+     * Sum all numbers in the array.
+     *
+     * @throws {TypeError}
+     *
+     * @param {array} The input array
+     * @return {integer} The result sum
+     */
+    sum: function arraySum(arr) {
+        return arr.reduce((acc, cur) => acc + cur, 0)
+    },
 }
 
 module.exports = {
