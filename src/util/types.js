9476962595a3b9d3a1dd6dab76d5d79c59914855
Doug Owings <doug@dougowings.net>
2021-08-08 22:54:07 -0700

    add annotations

--- src/util/types.js
+++ src/util/types.js
@@ -58,6 +58,14 @@
  */
 const {EventEmitter} = require('events')
 
+/**
+ * Returns a useful type value for the parameter. The default is to return the
+ * value of `typeof arg`, else one of the following: 'array', 'buffer', 'class',
+ * 'object', 'null', or 'stream'.
+ *
+ * @param {*} The parameter to check
+ * @return {string} The type
+ */
 function typeOf(arg) {
     if (arg === null) {
         return 'null'
@@ -71,6 +79,9 @@ function typeOf(arg) {
     if (Is.Stream(arg)) {
         return 'stream'
     }
+    if (Is.Class(arg)) {
+        return 'class'
+    }
     if (Is.Object(arg)) {
         return 'object'
     }
@@ -78,11 +89,16 @@ function typeOf(arg) {
 }
 
 const Cast = {
-   /**
-    * 
-    * @param {...*}
-    * @return {array}
-    */
+
+    /**
+     * Cast a parameter to an array. If the parameter is an array, the parameter
+     * is returned, otherwise a new array is created. If it is null or undefined,
+     * an empty array is returned, otherwise a singleton array with the parameter
+     * is return.
+     *
+     * @param {*} The parameter to cast
+     * @return {array} The parameter if it is an array, or a new array
+     */
     toArray: function castToArray(val) {
         if (Is.Array(val)) {
             return val
@@ -97,18 +113,44 @@ const Cast = {
 
 const Is = {
 
+    /**
+     * Whether the parameter is an array.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Array: function isArray(arg) {
         return Array.isArray(arg)
     },
 
+    /**
+     * Whether the parameter is a boolean. Returns false if it was constructed
+     * with the `new` keyword.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Boolean: function isBoolean(arg) {
         return arg === true || arg === false
     },
 
+    /**
+     * Whether the parameter is a buffer.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Buffer: function isBuffer(arg) {
         return Buffer.isBuffer(arg)
     },
 
+    /**
+     * Whether the parameter is probably a class. This tries to determine
+     * whether something *must* be constructed with the `new` keyword.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Class: function isClass(arg) {
         if (Is.Function(arg) === false) {
             return false
@@ -124,33 +166,73 @@ const Is = {
         return str.indexOf('Cannot call a class as a function') > -1
     },
 
+    /**
+     * Whether the parameter is an instance of Error.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Error: function isError(arg) {
         return arg instanceof Error
     },
 
+    /**
+     * Whether the parameter is a function.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Function: function isFunction(arg) {
         return typeof arg === 'function'
     },
 
+    /**
+     * Whether the parameter is iterable. NB: an object is not iterable.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Iterable: function isIterable(arg) {
         return arg != null && Is.Function(arg[Symbol.iterator])
     },
 
     /**
+     * Whether the parameter is a number.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
+    Number: function isNumber(arg) {
+        return typeof arg === 'number'
+    },
+    /**
+     * Whether the parameter is an object.
+     *
+     * Returns false for: buffer, null, function, new String/Boolean
+     * Returns true for: stream
+     *
      * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
      *
      * Copyright (c) 2014-2017, Jon Schlinkert.
      * Released under the MIT License.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
      */
     Object: function isObject(arg) {
         return Object.prototype.toString.call(arg) === '[object Object]'
     },
 
     /**
+     * Whether the parameter is probably a "plain" object.
+     *
      * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
      *
      * Copyright (c) 2014-2017, Jon Schlinkert.
      * Released under the MIT License.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
      */
     PlainObject: function isPlainObject(o) {
         let ctor
@@ -176,26 +258,63 @@ const Is = {
         return true
     },
 
+    /**
+     * Whether the parameter is a readable stream.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     ReadableStream: function isReadableStream(arg) {
         return arg instanceof EventEmitter && Is.Function(arg.read)
     },
 
+    /**
+     * Whether the parameter is a stream.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Stream: function isStream(arg) {
         return Is.ReadableStream(arg) || Is.WriteableStream(arg)
     },
 
+    /**
+     * Whether the parameter is a string. Returns false if it was constructed
+     * with the `new` keyword.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     String: function isString(arg) {
         return typeof arg === 'string'
     },
 
+    /**
+     * Whether the parameter is a Symbol.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     Symbol: function isSymbol(arg) {
         return typeof arg === 'symbol'
     },
 
+    /**
+     * Alias for `isWriteableStream()`.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     WritableStream: function isWritableStream(arg) {
         return Is.WriteableStream(arg)
     },
 
+    /**
+     * Whether the parameter is a writable (writeable) stream.
+     *
+     * @param {*} The parameter to check
+     * @return {boolean} The result
+     */
     WriteableStream: function isWriteableStream(arg) {
         return (
             arg instanceof EventEmitter &&
